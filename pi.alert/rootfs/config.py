8dc0b4f407975b5043af0c301f239dcd163a9cd4
danveitch76 <45300973+danveitch76@users.noreply.github.com>
2023-12-17 10:19:43 +0000

    Update Pi.Alert Add-on Folders

--- pi.alert/rootfs/config.py
+++
@@ -1,6 +0,0 @@
-"""
-This file is for development purposes.
-The deployment version won't take it to the container. Instead, run.sh will
-generate one with the options provided by bashio.
-"""
-DEBUG_MODE = False
